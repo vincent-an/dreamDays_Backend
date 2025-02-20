80cf59ee54004ea429d5d2260faad9bc7d997df3
vincent-an <xxv211@naver.com>
2025-02-21 04:44:44 +0900

    add config

--- src/main/java/dreamdays/Helf/config/CorsConfig.java
+++ src/main/java/dreamdays/Helf/config/CorsConfig.java
@@ -1,2 +1,31 @@
-package dreamdays.Helf.config;public class CorsConfig {
+package dreamdays.Helf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:3000" //리액트 로컬
+//                "https://mingle-mu.vercel.app/" //프론트배포를 하면 그 도메인을 넣으면 됨
+        ));
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("Set-Cookie");
+        config.addExposedHeader("Authorization");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }

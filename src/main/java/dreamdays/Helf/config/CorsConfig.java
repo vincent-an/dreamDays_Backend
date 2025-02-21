e0b29937540bc6307fe52821edfdba3709571c9b
vincent-an <xxv211@naver.com>
2025-02-22 03:30:11 +0900

    Update CorsConfig

--- src/main/java/dreamdays/Helf/config/CorsConfig.java
+++ src/main/java/dreamdays/Helf/config/CorsConfig.java
@@ -18,8 +18,8 @@ public class CorsConfig {
                 //리액트 로컬
                 "http://localhost:3000",
                 //프론트배포를 하면 그 도메인을 넣으면 됨
-                "https://eulji-hf.netlify.app"
-//                "http://127.0.0.1:5500"
+                "https://eulji-hf.netlify.app",
+                "http://127.0.0.1:5500"
         ));
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
